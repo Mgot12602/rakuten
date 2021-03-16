e8c5231683c6379eebc865135bca79f88d764cf6
Mgot12602 <marcgotzens@hotmail.com>
2021-03-16 18:52:25 +0100

    some comments in rakuten api part

--- src/services/rakutenapi.js
+++ src/services/rakutenapi.js
@@ -14,38 +14,33 @@ function ServerError(err) {
     };
   }
   
-  function successStatus(res) {
-    return {
-      status: true,
-      data: res.data,
-    };
-  }
-
+ 
+// defined the base and common urls
 const RAKUTEN_API_LIST_BASE_URL = 'https://gizmo.rakuten.tv/v3/';
-//   https://gizmo.rakuten.tv/v3/lists/estrenos-imprescindibles-en-taquilla?classification_id=5&device_identifier=web&locale=es&market_code=es
-  
+
 const RAKUTEN_API_COMMON_URI = '?classification_id=5&device_identifier=web&locale=es&market_code=es';
 
+// used axios to make to api calls
 const listService = axios.create({
     baseURL: RAKUTEN_API_LIST_BASE_URL
 });
-
+// api call to get each movie list
 export function getList(category) {
     return listService
-        .get(`lists/${category}?classification_id=5&device_identifier=web&locale=es&market_code=es`)
+        .get(`lists/${category}${RAKUTEN_API_COMMON_URI}`)
         .then((res) => {
           return res.data.data.contents.data;
         })
         .catch(ServerError);
 
 }
-
+// api call to get the movie details
 export function getMovie(movie) {
  
   return listService
-      .get(`movies/${movie}?classification_id=5&device_identifier=web&locale=es&market_code=es`)
+      .get(`movies/${movie}${RAKUTEN_API_COMMON_URI}`)
       .then((res) => {
-        console.log("Movie from server", res.data.data)
+       
         return res.data.data;
         
       })
